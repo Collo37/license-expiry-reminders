5f7b8ed91fabfad1206615171685a0740390ba5c
Collo37 <cowlduor37@gmail.com>
2023-01-26 18:23:57 +0300

    testing meil

--- src/globalFunctions/addCronJob.js
+++ src/globalFunctions/addCronJob.js
@@ -5,7 +5,7 @@ import sendMail from "./sendMail";
 
 export default async function addCronJob() {
   // scheduling a job
-  let task = cron.schedule("10 18 * * * *", async () => {
+  let task = cron.schedule("* 1 * * * *", async () => {
     const client = await MongoClient.connect(process.env.DATABASE_URL, {
       family: 4,
     });
